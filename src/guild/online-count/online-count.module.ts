a2aeabf1496721ddbf617040bb2140055ef26135
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2025-02-13 00:56:01 +0800

    refactor: moving online-count schema into shared for reuse

--- src/guild/online-count/online-count.module.ts
+++ src/guild/online-count/online-count.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OnlineCountController } from './online-count.controller';
 import { OnlineCountService } from './online-count.service';
-import { GuildOnlineCount, GuildOnlineCountSchema } from './online-count.schema';
+import { GuildOnlineCount, GuildOnlineCountSchema } from '@shared/schemas/online-count.schema';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: GuildOnlineCount.name, schema: GuildOnlineCountSchema }])],
--- src/guild/online-count/online-count.service.ts
+++ src/guild/online-count/online-count.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { GuildOnlineCount } from './online-count.schema';
+import { GuildOnlineCount } from '@shared/schemas/online-count.schema';
 
 @Injectable()
 export class OnlineCountService {
